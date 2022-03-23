9c54fb8cb8d1a61222ef6e80c2a2aba68d5781f1
zzs52 <1214734414@qq.com>
2022-03-23 23:10:57 +0800

    add new knowledge points

--- src/class02_strategy/comparator/Comparator.java
+++ src/class02_strategy/comparator/Comparator.java
@@ -1,6 +1,11 @@
 package class02_strategy.comparator;
 
+@FunctionalInterface
 public interface Comparator<T> {
 
     int compare(T o1, T o2);
+
+    default void sayHello() {
+        System.out.println("hello world!");
+    }
 }
--- src/class02_strategy/comparator/Main.java
+++ src/class02_strategy/comparator/Main.java
@@ -17,7 +17,15 @@ public class Main {
         // System.out.println(Arrays.toString(dogs));
         Cat[] cats = {new Cat(3, 3), new Cat(5, 5), new Cat(1, 1)};
         Sorter<Cat> sorter = new Sorter<>();
-        sorter.sort(cats, new CatHeightComparator());
+        sorter.sort(cats, (o1, o2)->{
+            if (o1.weight < o2.weight) {
+                return -1;
+            } else if (o1.weight > o2.weight) {
+                return 1;
+            } else {
+                return 0;
+            }
+        });
         System.out.println(Arrays.toString(cats));
     }
 }
